548e098361ba10babdd026410868cabf636d4053
kimomasterpro <aldo.rodriguez9761@alumnos.udg.mx>
2026-05-22 21:08:47 -0600

    Update main.c

--- actividad_02_Sistema_de_evaluacion_academica_avanzado/codigo/main.c
+++ actividad_02_Sistema_de_evaluacion_academica_avanzado/codigo/main.c
@@ -1,1 +1,26 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nm;
+    printf("ingresa el numero\n");
+    scanf("%i", &nm);
+
+    if (nm>1 && nm<=10){
+        if (nm==1||nm==2||nm==3||nm==5||nm==7){
+
+            printf ("es primo");
+
+        }
+        else {
+            printf ("el numero no es primo");
+        }}
+    else
+        {
+        printf ("el numero esta fuera de rango ");
+        }
+
+    return 0;
+}
