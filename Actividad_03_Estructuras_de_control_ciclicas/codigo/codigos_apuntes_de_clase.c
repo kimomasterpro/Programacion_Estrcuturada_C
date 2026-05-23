d20843e57ae29ef9438204f7b291eb30f21a7b46
kimomasterpro <aldo.rodriguez9761@alumnos.udg.mx>
2026-05-22 21:38:23 -0600

    codigos hechos en clase y anotados como apuntes
    Commented out code related to summing numbers.

---
+++ Actividad_03_Estructuras_de_control_ciclicas/codigo/codigos_apuntes_de_clase.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   
+   int x;
+   int suma =0;
+
+   printf("escribe un numero: ");
+   scanf("%d", &x);
+
+   while(x !=0){
+        suma +=x;
+        scanf("%d", &x);
+   }
+    
+
+    /*
+    int opcion;
+
+    do{
+        printf("1. continuar\n");
+        printf("2. salir\n");
+        scanf("%d", &opcion);
+    } while (opcion !=2);
+    */
+}
+  /*
+ for (int i = 1; i <= N; i++) {
+        suma += valor;
+    }
+*/
