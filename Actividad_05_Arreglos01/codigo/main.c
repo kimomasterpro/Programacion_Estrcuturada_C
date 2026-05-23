8b4545e993eff49188fcf0d027555d89bda90ddb
kimomasterpro <aldo.rodriguez9761@alumnos.udg.mx>
2026-05-23 11:12:32 -0600

    Codigo de Temperaturas usando matrices

--- Actividad_05_Arreglos01/codigo/main.c
+++ Actividad_05_Arreglos01/codigo/main.c
@@ -19,32 +19,4 @@ int main(){
     return 0;
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-
-
-    int N=4;
-    float temp [N];
-    int i;
-
-
-
-
-    for( i=0 ; i<N; i++){
-
-        printf("ingresa la temperatura de %d:\n", i);
-        scanf("%f", &temp[i]);
-
-    }
-
-    for( i=0 ; i<N; i++){
-
-        printf("las tempreraturas ingresadas fueron:%f\n", temp[i]);
-
-    }
-
-
-
 
