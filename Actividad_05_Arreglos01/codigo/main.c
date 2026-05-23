8058857c8eb7c4ebf2b7a4ad6ca3667531559261
kimomasterpro <aldo.rodriguez9761@alumnos.udg.mx>
2026-05-23 00:44:35 -0600

    Arreglos para direcciones de memoria y temperatura

--- Actividad_05_Arreglos01/codigo/main.c
+++ Actividad_05_Arreglos01/codigo/main.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    float temp[24];
+    float temp1, temp2;
+    int dummy;
+
+
+    printf("direccion de mem de dos flotantes: &temp1: %i, \n&temp2 %i!\n", &temp1, &temp2);
+    printf("\ndireccion de temp[24]: %i,\n", &temp);
+    printf("\ndireccion de &temp[24]: %i,\n", &temp[0]);
+    printf("\ndireccion de &dummy:%i\n", &dummy);
+
+
+
+
+    return 0;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+    int N=4;
+    float temp [N];
+    int i;
+
+
+
+
+    for( i=0 ; i<N; i++){
+
+        printf("ingresa la temperatura de %d:\n", i);
+        scanf("%f", &temp[i]);
+
+    }
+
+    for( i=0 ; i<N; i++){
+
+        printf("las tempreraturas ingresadas fueron:%f\n", temp[i]);
+
+    }
+
+
+
 
