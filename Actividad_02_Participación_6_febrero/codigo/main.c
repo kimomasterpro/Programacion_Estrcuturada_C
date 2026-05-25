edf31d9e9605981414fc6dc69892c512e145f751
kimomasterpro <aldo.rodriguez9761@alumnos.udg.mx>
2026-05-24 22:29:03 -0600

    algoritmo de evaluacion de expresiones

--- Actividad_02_Participación_6_febrero/codigo/main.c
+++ Actividad_02_Participación_6_febrero/codigo/main.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+
+int main(){
+
+
+    int a = 3;
+    int b = 4;
+    int c = a++ + --b;
+
+
+        printf("%d %d %d\n", a, b , c); // a = 4, b = 3, c = 6.
+
+
+    int x = 10;
+    int y = 3;
+
+        printf("%d\n %d\n", x + y * 2, (x + y) * 2); // x + y * 2 = 16 , (x + y) * 2 = 26.
+
+    return 0;
+}
